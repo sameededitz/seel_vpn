b3562d510787789d150069bb5082a9dd59657941
M. Sameed <sameedhassan22@gmail.com>
2025-04-24 16:48:25 +0500

    Update login.blade.php to use new logo

--- resources/views/livewire/auth/login.blade.php
+++ resources/views/livewire/auth/login.blade.php
@@ -8,7 +8,7 @@
                         <div class="card-body">
                             <div class="p-4">
                                 <div class="mb-3 text-center">
-                                    <img src="{{ asset('assets/images/logo-img.png') }}" width="160px" alt="logo" />
+                                    <img src="{{ asset('assets/images/logo-icon.png') }}" width="80px" alt="logo" />
                                 </div>
                                 <div class="text-center mb-4">
                                     <p class="mb-0">Please log in to your account</p>
