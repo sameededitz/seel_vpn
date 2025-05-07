f0fbd426d34e5520734c3fbcb778d9d2a1ed1674
Sameed <sameedhassan22@gmail.com>
2025-05-07 15:08:17 +0500

    redo all the code

--- resources/views/components/alert.blade.php
+++
@@ -1,4 +0,0 @@
-<div class="alert alert-{{ $type }} border-0 alert-dismissible fade show">
-    <div class="text-white"> {{ $message }} </div>
-    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-</div>
